dced8029df4a18903581dbe0dec1233207aff630
AlisaPutriAmalia <aliyaamalia0908@gmail.com>
2026-05-04 23:43:28 +0800

    Add biodata endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
     return response()->json(['message' => 'OK']);
+});
+
+Route::get('biodata', function () {
+    return response()->json([
+        'nama' => 'Alisa_Putri_Amalia_',
+        'nim' => '60200124063',
+        'kelas' => 'B'
+    ]);
 });
